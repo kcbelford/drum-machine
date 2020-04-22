500ec17bcd23539bdb88921b8e41221d9278ba1e
kcbelford <kcbelford@hotmail.com>
2020-04-22 15:49:58 -0700

    swapped and shortened audio files

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-  const cowbell = new Audio("./assets/audio/808-Cowbell1.wav");
+  const cowbell = new Audio("./assets/audio/808-Cowbell1-d.wav");
   $('#cowbell').click(e => cowbell.play());
 
-  const clap = new Audio("./assets/audio/808-Clap02.wav");
+  const clap = new Audio("./assets/audio/808-Clap02-e.wav");
   $('#clap').click(e => clap.play());
 
-  const clave = new Audio("./assets/audio/808-Clave1.wav");
+  const clave = new Audio("./assets/audio/808-Clave1-c.wav");
   $('#clave').click(e => clave.play());
 
-  const hiHatClosed = new Audio("./assets/audio/808-HiHats01.wav");
+  const hiHatClosed = new Audio("./assets/audio/808-HiHats01-b.wav");
   $('#hihatclosed').click(e => hiHatClosed.play());
 
-  const kick = new Audio("./assets/audio/808-Kicks01.wav");
+  const kick = new Audio("./assets/audio/808-Kicks05-b.wav");
   $('#kick').click(e => kick.play());
 
-  const hiHatOpened = new Audio("./assets/audio/808-OpenHiHats05.wav");
+  const hiHatOpened = new Audio("./assets/audio/808-OpenHiHats05-b.wav");
   $('#hihatopened').click(e => hiHatOpened.play());
 
-  const snare = new Audio("./assets/audio/808-Snare01.wav");
+  const snare = new Audio("./assets/audio/808-Snare05-c.wav");
   $('#snare').click(e => snare.play());
 
-  const tom = new Audio("./assets/audio/808-Tom2.wav");
-  $('#tom').click(e => tom.play());
+  const conga = new Audio("./assets/audio/808-Conga4.wav");
+  $('#conga').click(e => conga.play());
 
   console.log("working?");
 });
