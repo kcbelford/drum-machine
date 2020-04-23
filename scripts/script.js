078047404f19dcb44e3d2345f00a485a83ee189b
kcbelford <kcbelford@hotmail.com>
2020-04-22 20:20:21 -0700

    updated js with .load method

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,59 @@
 $(document).ready(function() {
-  const cowbell = new Audio("./assets/audio/808-Cowbell1-d.wav");
-  $('#cowbell').click(e => cowbell.play());
+  const cowbell = new Audio("./assets/audio/808-Cowbell1.wav");
+  // $('#cowbell').click(e => cowbell.play());
+  $('#cowbell').mousedown(function() {
+    cowbell.load();
+    cowbell.play();
+  });
 
-  const clap = new Audio("./assets/audio/808-Clap02-e.wav");
-  $('#clap').click(e => clap.play());
+  const clap = new Audio("./assets/audio/808-Clap02.wav");
+  // $('#clap').click(e => clap.play());
+  $('#clap').mousedown(function() {
+    clap.load();
+    clap.play();
+  });
 
-  const clave = new Audio("./assets/audio/808-Clave1-c.wav");
-  $('#clave').click(e => clave.play());
+  const clave = new Audio("./assets/audio/808-Clave1.wav");
+  // $('#clave').click(e => clave.play());
+  $('#clave').mousedown(function() {
+    clave.load();
+    clave.play();
+  });
 
-  const hiHatClosed = new Audio("./assets/audio/808-HiHats01-b.wav");
-  $('#hihatclosed').click(e => hiHatClosed.play());
+  const hiHatClosed = new Audio("./assets/audio/808-HiHats01.wav");
+  // $('#hihatclosed').click(e => hiHatClosed.play());
+  $('#hihatclosed').mousedown(function() {
+    hiHatClosed.load();
+    hiHatClosed.play();
+  });
 
-  const kick = new Audio("./assets/audio/808-Kicks05-b.wav");
-  $('#kick').click(e => kick.play());
+  const kick = new Audio("./assets/audio/808-Kicks05.wav");
+  // $('#kick').click(e => kick.play());
+  $('#kick').mousedown(function() {
+    kick.load();
+    kick.play();
+  });
 
-  const hiHatOpened = new Audio("./assets/audio/808-OpenHiHats05-b.wav");
-  $('#hihatopened').click(e => hiHatOpened.play());
+  const hiHatOpened = new Audio("./assets/audio/808-OpenHiHats05.wav");
+  // $('#hihatopened').click(e => hiHatOpened.play());
+  $('#hihatopened').mousedown(function() {
+    hiHatOpened.load();
+    hiHatOpened.play();
+  });
 
-  const snare = new Audio("./assets/audio/808-Snare05-c.wav");
-  $('#snare').click(e => snare.play());
+  const snare = new Audio("./assets/audio/808-Snare05.wav");
+  // $('#snare').click(e => snare.play());
+  $('#snare').mousedown(function() {
+    snare.load();
+    snare.play();
+  });
 
   const conga = new Audio("./assets/audio/808-Conga4.wav");
-  $('#conga').click(e => conga.play());
+  // $('#conga').click(e => conga.play());
+  $('#conga').mousedown(function() {
+    conga.load();
+    conga.play();
+  });
 
   console.log("working?");
 });
